be2732507a49d486192afe3b3ecd89901bf21fd4
Leonard Adjei <82063118+blameleo@users.noreply.github.com>
2022-06-08 21:30:32 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+let input = document.getElementById('inputPlace')
+const searchBtn = document.getElementById('searchBtn')
+const display = document.querySelector('.test')
+
+
+function hello() {
+    console.log('no');
+}
+
+
+
+ async function getMovieApi() {
+    const res = await fetch(`http://www.omdbapi.com/?s=${input.value}&apikey=82ecaef9`)
+    const data = await res.json()
+            for(let i=0; i<10; i++){
+                const movies = data.Search[i]
+                fetch(`http://www.omdbapi.com/?i=${movies.imdbID}&apikey=82ecaef9`)
+                .then (res => res.json())
+                .then (info => {
+                    console.log(info)
+                    display.innerHTML +=`
+                    <div class="display">
+                    <div class="posterImage">
+                    <img src='${info.Poster}'>
+                </div>
+                
+                <div class="content">
+                    <div class='titleHeader'>
+                    <h2 class="posterTitle">${info.Title}</h2>
+                    <p>⭐${info.Ratings[0].Value} </p>
+        
+                    </div>
+                    
+                    <p class='subtext'>${info.Runtime}   <span class='genre'> ${info.Genre}<span></p>
+                    <button id="watchlistBtn" >+</button>
+                    <p class='plot'>${info.Plot}</p>
+                </div>
+        
+                    </div>
+        
+        
+                    `
+                })
+
+                input.value = ""
+
+               display.innerHTML = ``
+            }
+
+           
+}  
+
+    
+
+searchBtn.addEventListener('click', getMovieApi)
+
+document.getElementById('watchlistBtn').addEventListener('click', hello)
+
